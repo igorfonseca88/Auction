18a539f267c55bdedc13a657358ac88b9df372f9
Thiago Araújo Berbet <thiago.berbet@gmail.com>
2012-03-05 11:28:28 -0300

    Alterações na conta de usuario
    Signed-off-by: Thiago Araújo Berbet <thiago.berbet@gmail.com>

---
+++ otimolance/config/email.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| EMAIL CONFING
+| -------------------------------------------------------------------
+| Configuration of outgoing mail server.
+| */
+$config['protocol']='smtp';
+//$config['smtp_host']='smtp.gmail.com';
+//$config['smtp_port']='587';
+$config['smtp_timeout']='60';
+//$config['smtp_user']='otimolance@gmail.com';
+$config['smtp_pass']='wmc749fr';
+$config['charset']='utf-8';
+$config['newline']="\r\n";
+
+?>
