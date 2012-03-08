6cec6d92d2bdb6c4b6d3d5fac53ffb4deedd6df8
William Witter da Silva <wwwitters@gmail.com>
2012-03-07 21:35:10 -0300

    Integração com pagseguro
    Signed-off-by: William Witter da Silva <wwwitters@gmail.com>

--- otimolance/controllers/compraController.php
+++ otimolance/controllers/compraController.php
@@ -1,4 +1,5 @@
 <?php
+require_once "PagSeguroLibrary/PagSeguroLibrary.php";
 
 class CompraController extends CI_Controller {
 
@@ -23,8 +24,57 @@ class CompraController extends CI_Controller {
      function pagamento(){
         $this->load->model("Produto_model", "produtoDAO");
         $idProduto = $this->input->post("idProdutoHidden");
+        $quantidade = $this->input->post("txtQuantidade");
         $data["produtos"] = $this->produtoDAO->buscarProdutosGaleriaPorIdProduto($idProduto);
+        
+        $dadosNovaCompra = array(
+                   'produtos'  => $this->produtoDAO->buscarProdutosGaleriaPorIdProduto($idProduto),
+                   'quantidade'     => $quantidade
+               );
+        $this->session->set_userdata($dadosNovaCompra);      
         $this->load->view("compra/pagamento",$data);
+     }
+     
+     function criarTransacaoPagSeguro(){
+         $quantidade = $this->session->userdata('quantidade');
+         $produtos = $this->session->userdata('produtos');
+         
+	$paymentRequest = new PagSeguroPaymentRequest();
+	//moeda Brasileira
+        $paymentRequest->setCurrency("BRL");
+		
+        foreach ($produtos as $row) {
+            $paymentRequest->addItem($row->idProduto, $row->nome,  100);
+        }
+	
+	//serve para controles futuros, caso estejamos salvando em banco também as transações
+	//$paymentRequest->setReference("REF1234");
+	
+        //entrega FRETE
+        $CODIGO_SEDEX = PagSeguroShippingType::getCodeByType('SEDEX');
+	$paymentRequest->setShippingType($CODIGO_SEDEX);
+	$paymentRequest->setShippingAddress('01452002',  'Av. Brig. Faria Lima',  '1384', 'apto. 114', 'Jardim Paulistano', 'São Paulo', 'SP', 'BRA');
+	
+	//dados do cliente para que ele não necessite informar novamente no pagseguro
+	$paymentRequest->setSender('William Witter da Silva Comprador', 'wwwitters@gmail.com', '11', '56273440');
+	
+	$paymentRequest->setRedirectUrl("http://www.lojamodelo.com.br");
+	
+	try {
+		
+		/*
+		* #### Crendencials ##### 
+		* Credenciais (e-mail and token) - Token = numero gerado na conta do pagseguro
+		* Você pode pegar as credenciais de um arquivo de conf.
+		* $credentials = PagSeguroConfig::getAccountCredentials();
+		*/			
+		$credentials = new PagSeguroAccountCredentials("wwwitters@gmail.com", "2C36B0FEB8844C62947D65145B09115A");
+		
+		$url = $paymentRequest->register($credentials);
+		
+	} catch (PagSeguroServiceException $e) {
+		die($e->getMessage());
+	}
      }
 }
 
--- otimolance/views/compra/pagamento.php
+++ otimolance/views/compra/pagamento.php
@@ -1,17 +1,5 @@
 <div>
- <form target="pagseguro" method="post" action="https://pagseguro.uol.com.br/checkout/checkout.jhtml">
-  <input type="hidden" name="email_cobranca" value="suporte@lojamodelo.com.br" />
-  <input type="hidden" name="tipo" value="CP" />
-  <input type="hidden" name="moeda" value="BRL" />
-  <input type="hidden" name="item_id" value="1" />
-  <input type="hidden" name="item_descr" value="Descrição do item a ser vendido" />
-  <input type="hidden" name="item_quant" value="1" />
-  <input type="hidden" name="item_valor" value="100" />
-  <input type="hidden" name="frete" value="0" />
-  <input type="hidden" name="peso" value="0" />
-  <input type="image" name="submit" 
-  src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/pagamentos/99x61-comprar-assina.gif"
-  alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
+<form target="pagseguro" action="criarTransacaoPagSeguro" method="post">
+    <input type="image" src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/pagamentos/209x48-pagar-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
 </form>
-    </div>
 </div>
