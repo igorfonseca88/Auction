792774bb4048063f1fd5a2796cb42a995b8f30e6
Thiago Araújo Berbet <thiago.berbet@gmail.com>
2012-03-13 20:15:18 -0400

    Alteração de conta de usuario
    Signed-off-by: Thiago Araújo Berbet <thiago.berbet@gmail.com>

---
+++ otimolance/views/conta/recuperarSenha.php
@@ -0,0 +1,14 @@
+<?
+$this->load->view('_padrao/topo');
+?>
+<br/>
+<br/>	
+<div class="formulario">
+    <?=$sucesso != "" ? '<div class="success"> ' . $sucesso . ' </div>' : "" ?>
+    <?= $erro != "" ? '<div class="error"> ' . $erro . ' </div>' : "" ?>      
+    <form method="post" action="<?= BASE_URL(); ?>contaController/retornarSenha">
+            <label>Digite seu e-mail</label><br />
+            <input type="text" name="txtEmail" id="txtEmail" value="" class="input"/>
+            <input type="submit" value="Enviar" class="button"/>
+    </form>
+</div>
