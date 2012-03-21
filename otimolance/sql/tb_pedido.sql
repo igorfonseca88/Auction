bef908e00adfcf67160f11e53c45806f06f3a12f
Igor Fonseca Paixao <igorfonseca88@gmail.com>
2012-03-20 20:27:36 -0400

    incluida colunas dataCriacao para armazenar a data de criacao do pedido e idLeilao para pedidos que serão gerados de um leilao arrematado

--- otimolance/sql/tb_pedido.sql
+++ otimolance/sql/tb_pedido.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 09/03/2012 às 00h56min
+-- Tempo de Geração: 20/03/2012 às 20h25min
 -- Versão do Servidor: 5.5.16
 -- Versão do PHP: 5.3.8
 
@@ -28,11 +28,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `tb_pedido` (
   `idPedido` int(11) NOT NULL AUTO_INCREMENT,
-  `idItemPedido` int(11) NOT NULL,
   `total` float NOT NULL,
   `desconto` decimal(10,0) NOT NULL,
+  `idConta` int(11) NOT NULL,
+  `idLeilao` int(11)  NULL,
+  `status` varchar(20) NOT NULL,
+  `dataCriacao` datetime NOT NULL,
   PRIMARY KEY (`idPedido`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+--
+-- Extraindo dados da tabela `tb_pedido`
+--
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
